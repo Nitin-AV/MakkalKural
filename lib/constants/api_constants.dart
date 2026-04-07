fdc96709c222230a913ff46cb233b22c179e7eed
Nitin A V <nitin@macsit.net>
2026-04-07 20:51:58 +0530

    Api Fix

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,9 +1,9 @@
 class ApiConstants {
   static const String supabaseUrl =
-      "https://wriwcwwnywfqyqvjdvod.supabase.co";
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.supabase.co";
       
   static const String supabaseAnonKey =
-      "sb_publishable_YzjyQc2FqNhqV3rTw2yEgg_VSjytA7S";
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 
 }
